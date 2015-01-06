6796d43705829234b1445c645852ac88d477bbff
Charlie Su <dragonsmirth@gmail.com>
2015-01-05 23:23:44 -0800

    Made a few size changes. Experimenting with metadata and animation

---
+++ js/buttons.js
@@ -0,0 +1,5 @@
+var buttons = [document.querySelector(".one"), document.querySelector(".two"),document.querySelector(".three"),document.querySelector(".four")];
+for(i = 0; i < buttons.length; i++)
+{
+	buttons[i].addEventListener('click', function(){})
+}
