f45e6e90eb50603adcc1ae4955db6a4e37017e76
Charlie Su <dragonsmirth@gmail.com>
2015-01-08 22:47:57 -0800

    Made a new, better, more representative favicon. Left message at index.html making it clear that none of the links are open

--- js/buttons.js
+++ js/buttons.js
@@ -1,6 +1,6 @@
 $(function() {
 	$(".one").tooltip({content:"Shameless self advertising here!", my: "center bottom-20", at:"center top", collision:"fit"});
-	$(".two").tooltip({content:"Interested in Purdue? Take a look at my experiences!", my: "right", at:"center", collision:"fit"});
+	$(".two").tooltip({content:"Interested in Purdue? Take a look at my experiences~", my: "right", at:"center", collision:"fit"});
 	$(".three").tooltip({content: "Look, <span class=\"strikeThru\">but don't</span> DO touch! Play with my projects!", my: "right", at:"center", collision:"fit" });
 	$(".four").tooltip({content: "Thoughts? Feedback? Ideas?", my: "right", at:"center", collision:"fit"});
 });
