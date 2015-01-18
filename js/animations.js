7aba610c2243e45f3fd203f868793e789a96ca5b
Charlie Su <dragonsmirth@gmail.com>
2015-01-17 22:10:36 -0500

    Added in font-changing. Also made the welcome message fade out and disappear upon clicking other buttons. Currently testing injecting html content to current page

--- js/animations.js
+++ js/animations.js
@@ -1,33 +1,51 @@
-var prevClass;
-var myButtons = [".one",".two",".three",".four"];
+function loadPage(myPage)
+{
+}
+
+/*This is for changing the color of the background when clicking on the menu buttons*/
+var myButtons = [".one",".two",".three",".four"];//used for tracking other buttons
+var myPages = ["about.html","courses.html","projects.html","contact.html"]
 function changeColor(myClass)
 {
-	var originalColor = $(myClass).css('background-color');
-	var hoverColor;
+	var originalColor = $(myClass).css('background-color');//original color
+	var hoverColor;//hovered color
 	$(myClass).click(function(){
-		if(!hoverColor)
-			hoverColor = $(myClass).css('background-color');
-		if($('html').css('background-color') != 'rgb(0,0,0)')
+		var currentIndex;
+		for(var index = 0; index < myButtons.length; index++)//decolor all other buttons
 		{
-
+			if(myButtons[index] != myClass && $(myButtons[index]).css('background-color') != originalColor)//second boolean to prevent hover color from defined as original
+			{
+				$(myButtons[index]).css('background-color', originalColor);
+				$(myButtons[index]).animate({fontSize: '100%'});
+			}
+			else
+				currentIndex = index;
 		}
-		if($('html').css('background-color') == hoverColor)
+		if(!hoverColor)//if hoverColor hasn't been defined before, define it here and never again
+		{
+			hoverColor = $(myClass).css('background-color');
+			$("#original").animate({opacity: '0'});//to make the title fade out			
+			$("#original").css('display', 'none');//to make the title fade out			
+		}
+		if($('html').css('background-color') == hoverColor)//if the current color is the same as clicked, turn off the lights
 		{
 			$('html').animate({backgroundColor: 'rgb(0,0,0)'},{duration: 600});
 			$(myClass).css('background-color', originalColor);
-			//$(myClass).animate({fontSize: '100%'});						
+			$(myClass).animate({fontSize: '100%'});
+			$("#original").css('display', 'block');//to make the title fade out			
+			$("#original").animate({opacity: '1'});	//back to black, so the title fades back in		
 		}
-		else
+		else//otherwise change color to new buton
 		{
 			$('html').animate({backgroundColor: hoverColor},{duration: 600})
 			$(myClass).css('background-color', hoverColor);
-			//$(myClass).animate({fontSize: '105%'});			
+			$(myClass).animate({fontSize: '105%'});	
+			$("#original").animate({opacity: '0'});//to make the title fade out
+			$("#original").css('display', 'none');//to make the title fade out
+			$("#content").load(myPages[currentIndex]);			
+			//$(myPages[currentIndex]).css('display', 'block');			
+			//$(myPages[currentIndex]).animate({display: '100'})
 		}
-		for(var index = 0; index < myButtons.length; index++)
-		{
-			if(myButtons[index] != myClass && $(myButtons[index]).css('background-color') != originalColor)
-				$(myButtons[index]).css('background-color', originalColor);
-		}		
 	})
 }
 
@@ -39,26 +57,3 @@ var main = function()
 	$('.four').click(changeColor('.four'));
 }
 $(document).ready(main);
-
-/*
-var main = function()
-{
-    var originalColor = $('li').css('background-color');//grabs non-hovered color
-    var hoverColor;
-    $('li').click(function()
-        {
-            if(!hoverColor)//if not declared
-                hoverColor = $('li').css('background-color');
-            if($('html').css('background-color') == hoverColor)
-            {
-                $('html').css('background-color', originalColor);
-                $('li').css('background-color', originalColor);
-            }
-            else
-            {
-                $('html').css('background-color', hoverColor);
-                $('li').css('background-color', hoverColor);
-            }
-        }
-                  )}
-$(document).ready(main);*/
